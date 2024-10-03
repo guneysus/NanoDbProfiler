30443457f9992fa534e05505522629cea0ceb6cd
Ahmed Seref Guneysu <949232+guneysus@users.noreply.github.com>
2024-10-03 23:05:37 +0300

    fun \o/

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -32,11 +32,24 @@ public class Program
 
         app.MapGet("/select/scalar", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Select(x => x.Id)));
 
+        app.MapGet("/select/no-tracking", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Take(10).AsNoTracking()));
+
         app.MapGet("/select/single/1", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.FirstOrDefault()));
 
         app.MapGet("/select/single/2", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Skip(1).FirstOrDefault()));
 
         app.MapGet("/select/all", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos));
+
+        app.MapGet("/select/max-id", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Max(x => x.Id)));
+        app.MapGet("/select/count", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Count()));
+        app.MapGet("/select/sum", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Sum(x => x.Id)));
+        app.MapGet("/select/orderby-desc", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.OrderByDescending(x => x.Id)));
+        app.MapGet("/select/avg", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Average(x => x.Id)));
+        app.MapGet("/select/starts-with", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Where(x => x.Title.StartsWith("x"))));
+        app.MapGet("/select/ends-with", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Where(x => x.Title.EndsWith("x"))));
+        app.MapGet("/select/contains", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Where(x => x.Title.Contains("x"))));
+        app.MapGet("/select/distinct-by", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.DistinctBy(x => x.Title)));
+        app.MapGet("/select/find", (HttpContext h, [FromServices] TodoContext db) => Results.Ok(db.Todos.Find(0)));
 
         app.MapGet("/update", (HttpContext h, [FromServices] TodoContext db) =>
         {
